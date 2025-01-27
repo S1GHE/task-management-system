7c434559104b14a22ac46534c11f2a006a03d67a
S1GHE <golikovefim65@gmail.com>
2025-01-27 23:30:03 +0300

    fix: handler UpdateProject

--- api/internal/applications/projects/update.go
+++ api/internal/applications/projects/update.go
@@ -1,3 +1,19 @@
 package projects
 
-func (h *ProjectHandler) UpdateProject()
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (h *ProjectHandler) UpdateProject(w http.ResponseWriter, _ *http.Request) {
+	data, err := json.Marshal(map[string]string{"msg": "Обновление проекта"})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
